a03f10a70bb4067b0c05d3662c8d8e72b541d9fc
epwr <ericpower@outlook.com>
2021-09-03 16:27:11 -0700

    Updated various pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,13 @@ function Home() {
     return (
         <div className="section flex-col">
             <p className="title-2">About Me</p>
-            <p>As a developer, I am passionate about automating workflows and protecting data. I believe that software can
-            help everyone increase the impact of their work, but that you need to build systems that do the basics properly. 
-            If your tool doesn't make someone's life easier, then they won't use it. If your tool exposes someone's private 
-            data, then they will stop using it. </p>
-            <p>As a human being, I care about how our technology affects our society. From democratizing knowledge to the rise of 
-            purposeful misinformation, we can see that technologies have broad and important effects. If you aren't thoughtful about
-            how a technology will be used, or aware of history, then all too often what you build will have negative effects on our 
-            society.</p>
+            <p>Hi! I'm Eric Power. I'm a software developer and all round nerd who likes talking about everything from 19th century history to the engineering behind gutters.
+            I'm perpetually curious, and try to create things that make the world a better place.</p>
+            <p>As a developer, I'm passionate about automating workflows and protecting data. I care about how people use the technology I create,
+            and I want my tools to make the world a better place. </p>
+            <p>Outside of the world of software, I have a host of hobbies I love. I read voraciously. I cycle as much as possible. I regularly get out of the city to hike and camp. 
+            Once this pandemic is over, I'm excited to get back to travelling. But at heart, my real love is learning. Recently I've gone on a deep dive into the post-Napoleonic War reconstruction period, but I also spend time 
+            researching statistics, economic and political theory, writing, the science of sleep, marketing, sociology, and more.</p>
             <p>I have a B.Sc. in Computer Science from the University of Victoria, where I focused on building software infrastructure and understanding the 
             effects that software systems can have on our society.</p>
         </div>
--- src/pages/Now.jsx
+++ src/pages/Now.jsx
@@ -4,10 +4,13 @@ function Now() {
             <p className="title-2">What I've Been Up To</p>
             <p>After wrapping up my degree, I took a short break where I worked on a couple projects and had some fun in nature! In the last two months I:</p>
             <ul>
-                <li>Wrote a custom programming language called <a href="https://github.com/epwr/lazy-linear-algebra">Lazy Linear Algebra</a>. I chose to write each part of it
-                from scratch, so there's a bespoke lexer, parser, interpreter, and REPL. Everything is written in Ruby 3 to let me experiment with Ruby 3's pattern matching features.</li>
-                <li>Designed and built a deck for my parents.</li>
-                <li>Read a bunch of books, mostly focusing on 19th century history, but also including some ancient Roman history, economics, political philosophy, and statistics.</li>
+                <li><p>Wrote a custom programming language called <a href="https://github.com/epwr/lazy-linear-algebra">Lazy Linear Algebra</a>. I chose to write each part of it
+                from scratch, so there's a bespoke lexer, parser, interpreter, and REPL. The language provides a clean and flexible syntax, and everything you need for basic quantum
+                computation - from complex number addition to tensor multiplication.</p>
+                <p>Lazy Linear Algebra is written in Ruby 3 to let me experiment with Ruby 3's pattern matching features.</p></li>
+                <li><p>Designed and built a deck for my parents. I'm very happy with how it turned out:</p>
+                <img className="main" src="/img/parents-back-deck.jpg"></img></li>
+                <li>Read a bunch of books, mostly focusing on 19th century history (specifically the post-Napoleonic War reconstruction period), but also including some ancient Roman history, economics, political philosophy, and statistics.</li>
                 <li>Spent time learning about <a className="external-link" href="http://paulgraham.com/weird.html">weird programming languages</a> (mostly Clojure) to get outside of
                 the standard concept of what programming is.</li>
             </ul>
@@ -15,6 +18,7 @@ function Now() {
             <p>I'm trying to figure out how I want to spend my time moving forwards. One important part of this is figuring out I want to do for work. I'm interested in software 
             infrastructure and DevOps, and am actively searching for the right job. The other is what side project to take on next. Most of the ideas on my to-do list 
             involve automating workflows, setting up open source tools on servers that I run, or building a product that I can share with the world.</p>
+            <p>Another big decision I'm working on is where to live. Right now I'm living in Victoria, BC, but I'd love to move back to Montreal for a while, or somewhere new!</p>
             <p>Last Updated: Sept 3, 2021</p>
         </div>
     );
