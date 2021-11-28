53f2956748ed4e956a213e12a66283bb122282e4
epwr <ericpower@outlook.com>
2021-11-28 12:17:48 -0800

    nov 28 update

--- src/pages/Now.jsx
+++ src/pages/Now.jsx
@@ -1,15 +1,13 @@
+
 function Now() {
     return (
         <div className="section flex-col">
-            <p><emph>Last Updated: Nov 27, 2021</emph></p>
+            <p><emph>Last Updated: Nov 28, 2021</emph></p>
 
 	    <p className="title-2">What I've Been Up To</p>
 
-	    <p>For the past year, I've been tinkering with the idea of a podcast. Less because I like the sound of my own voice, and more because I think that writing is one of the best ways to improve the clarity of thought; however, I find I love the medium of podcasting, and believe that I would enjoy the process of writing more if I were publishing it as a podcast. However, I can't find a good open source podcast server. Sorry RSS.com and all the other free podcast servers - I don't trust you with my data. I would absolutely support using <a href="https://transistor.fm">Transistor</a>, but it's more than I would want (I don't expect anyone to listen to my podcast, so I want an easy way to publish it that let's it live on the web). Or at least this is the tortured reasoning I'm following into my next project idea: an RSS server. I would love the ability to host one or more podcasts on my own server, and I'm extremely surprised that I can't find a half decent open source tool to do this.</p>
-
-	    <p>Oh well, that's the joy of being a software engineer; I get to build things I want. I've got a day scheduled for a personal hackathon on this and if I get anywhere I'll publish it.</p>
-		
-	    <p>Professionally, I started a new job at <a href="https://www.audette.io">Audette.io</a>, a startup that is automating the carbon reduction planning process for existing buildings. I've found the team to be amazing, and it's amazing to feel good about the purpose of your work. I've been working on building up our data ingestion pipeline, and am starting to spend time builing out components of our expert system.</p>
+	    <p>Probably the most noteworthy change in my life recently has been starting a new job with <a href="https://www.audette.io">Audette.io</a>. Audette is a startup that is automating the carbon reduction planning process for existing buildings, and I've been really enjoying my time with them. The team is amazing, the company's vision is something I feel good supporting, and I'm getting to tinker with new tools and technologies. I started by taking responsiblity of Audette's data ingestion pipeline, and now I'm adding R&amp;D to the role. I get to work alongside our team of building scientists to discover and prototype tools that can speed them up, or automate parts of their process.</p>
+	    <p>Outside of work, there are two ideas that I can't get out of my head: functional programming makes way more sense than OO for most problem domains, and why isn't there an open source tool for running your own RSS/podcast server. I've got a personal hackathon scheduled in my calendar to combine these two ideas by throwing together a v0.1 of an RSS/podcast server in Clojure (a functional programming language that runs on the JVM).</p>
 	    
         </div>
     );
