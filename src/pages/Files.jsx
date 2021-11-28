fe079e711620fb413d4e8d9aa8286fd473c8c2fc
epwr <ericpower@outlook.com>
2021-11-28 12:48:22 -0800

    updating the resume file & moving it to the bottom of the files page

--- src/pages/Files.jsx
+++ src/pages/Files.jsx
@@ -1,9 +1,6 @@
 function Files() {
     return (
         <div className="section">
-            <p className="title-2">Resume</p>
-            <p>If you're interested in some more specifics about my background, check out my <a href="/files/resume-ericpower.pdf">resume</a>.</p>
-
             <p className="title-2">Notes</p>
             <p>By the end of my degree, I had changed from writing notes by hand to taking notes in LaTeX. For some courses, I
             shared my notes with classmates. Because I wanted a place to link to, here they are:</p>
@@ -14,6 +11,10 @@ function Files() {
                 <li><a href="/files/seng480c-notes.pdf">SENG480C: Quantum Computing</a></li>
             </ul>
             <p>Feel free to let me know if you find anything that should be updated in these notes!</p>
+
+	    <p className="title-2">Resume</p>
+            <p>If you're interested in some more specifics about my background, check out my <a href="/files/resume-ericpower.pdf">resume</a>.</p>
+
         </div>
     );
 }
